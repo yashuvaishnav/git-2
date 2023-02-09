a8b4ffd7b1a68ecc435937a59df75027690637e3
unknown <dhirajkumarvicky9695@gmail.com>
2023-02-09 18:18:55 +0530

    prime number

--- array.js
+++ array.js
@@ -1,2 +1,12 @@
-let n = ["yashu","rishu","yogu","kittu","anshu"];
-console.log(n[1]);
+// prime number code
+
+let num=13;
+let count=0
+for(let i=0;i<num.length;i++)
+{
+    if(num%i==0)
+    {
+        count++
+    }
+}
+(count==2)?console.log("Prime number"):console.log("Not a Prime")
